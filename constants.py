359ddf010a1693ea25f37e8c2be9da0588e3d795
vasily-esipenko <esipenkovasilij@gmail.com>
2020-07-04 17:49:43 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,1 +1,5 @@
 token = "1071339939:AAHr4N_3P7QYj4jAk6aY5xjhxkNBeND****"
+
+social_networks = ["VK", "Instagram", "Twitter"]
+
+urls = {"VK": "https://vk.com/{0}", "Instagram": "https://www.instagram.com/{0}", "Twitter": "https://twitter.com/{0}"}
